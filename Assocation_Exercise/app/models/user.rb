f961dae204cc5f0b44be9872aa049e81529d66ae
your name here <stevedipietro97@gmail.com>
2019-12-19 17:27:53 -0500

    Finished Association Exercise

---
+++ Assocation_Exercise/app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ApplicationRecord
+    has_many :enrollments,
+        class_name: 'Enrollment',
+        primary_key: :id,
+        foreign_key: :student_id
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+
+    has_many :courses,
+        class_name: 'Course',
+        primary_key: :id,
+        foreign_key: :instructor_id
+    # Do we need belongs to? And is it implemented correctly
+
+    
+end
